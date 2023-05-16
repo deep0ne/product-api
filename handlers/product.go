95d351cfdccb91f702cec8b1cd71040b51feaeba
Николай Кожух <kozhukhovmib@gmail.com>
2023-05-16 20:16:27 +0300

    json tags

--- handlers/product.go
+++ handlers/product.go
@@ -17,5 +17,10 @@ func NewProducts(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
-	json.Marshal
+	d, err := json.Marshal(lp)
+	if err != nil {
+		http.Error(w, "Unnable to marshal JSON", http.StatusInternalServerError)
+	}
+
+	w.Write(d)
 }
