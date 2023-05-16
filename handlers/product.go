5ad4004158a3e2a2a7a1b155216857f85d70aca7
Alexander <kozhukhovmib@gmail.com>
2023-05-16 23:28:51 +0300

    implemented encoder

--- handlers/product.go
+++ handlers/product.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"product-api/data"
@@ -16,11 +15,18 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(w, r)
+		return
+	}
+
+	w.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
-	d, err := json.Marshal(lp)
+	err := lp.ToJSON(w)
 	if err != nil {
 		http.Error(w, "Unnable to marshal JSON", http.StatusInternalServerError)
 	}
-
-	w.Write(d)
 }
