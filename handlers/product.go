c0d27e8a2f4ed7b08d58c12692f61ef6fb024811
Николай Кожух <kozhukhovmib@gmail.com>
2023-05-18 14:32:50 +0300

    started implementing swagger

--- handlers/product.go
+++ handlers/product.go
@@ -1,3 +1,18 @@
+// package classification of ProductAPI
+
+// Documentation for Product API
+
+// Schemes: http
+// BasePath: /
+// Version: 1.0.0
+//
+// Consumes:
+// - application/json
+//
+// Produces:
+// - application/json
+// swagger:meta
+
 package handlers
 
 import (
